4e204f80bd8a5f4a316a81c63f384680d5a179f0
Ben Nolan <bennolan@Bens-MacBook-Air.local>
2023-12-06 23:06:32 -0500

    add some functionality to recipe component

--- recipebook/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipebook/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit{
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'A Description test', 'https://bakesbybrownsugar.com/wp-content/uploads/2022/01/Chocolate-Rugelach-2.jpg.webp'),
+    new Recipe('Another Test Recipe', 'Another Description test', 'https://bakesbybrownsugar.com/wp-content/uploads/2022/01/Chocolate-Rugelach-2.jpg.webp'),
+    new Recipe('A 3rd Test Recipe', 'A 3rd Description test', 'https://bakesbybrownsugar.com/wp-content/uploads/2022/01/Chocolate-Rugelach-2.jpg.webp'),
+    new Recipe('The last Test Recipe', 'The last Description test', 'https://bakesbybrownsugar.com/wp-content/uploads/2022/01/Chocolate-Rugelach-2.jpg.webp')
 
+  ];
+  constructor() {}
+
+  ngOnInit() {
+    
+  }
 }
